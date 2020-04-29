cb99b3ec26df5645786e3a7b8049d76ff1a8a363
gianca951 <gianca9405@gmail.com>
2020-04-29 12:43:11 -0300

    eje8 terminado, con un error

--- eje8.py
+++ eje8.py
@@ -20,18 +20,64 @@
 
 # !/usr/bin/python3
 
-import getopt
-#import os.path
-import sys
+import os, getopt, sys, signal
 
-(opt, arg) = getopt.getopt(sys.argv[1:], 'p:', ['process='])
 
-print('(Opciones ; Argumentos)', opt)
+def func():
+    (opt, arg) = getopt.getopt(sys.argv[1:], 'p:', ['process='])
 
-for (op, ar) in opt:
-    if (op in ['-p', '--process']):
-        proc = str(ar)
-        print("Cantidad de procesos a generar = ", ar)
+    print('(Opcion ; Argumentos)', opt)
 
-    else:
-        print("Opcion invalida")
+    # Creo un for para tomar la cantidad de procesos hijos a crear designados por el argumento ingresado
+    for (op, ar) in opt:
+        if (op in ['-p', '--process']):
+            # Alojo el argumento en la variable "proc"
+            proc = int(ar)
+            print("Cantidad de procesos hijos a generar:", proc)
+            print("PID del proceso PADRE:", os.getpid())
+            print("+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+            return proc
+
+
+# Defino "signals" para el envio de señales.
+def signals(child):
+    print("+-+-+-+-+-+-+-+-+-+-+-+-+-+")
+    for pid in child:
+        os.kill(pid, signal.SIGUSR2)
+    os.wait()
+
+
+# Definimos handler para el manejo de señales
+def handler(signal, frame):
+    print("Soy el PID", os.getpid(), "(PID DEL HIJO), recibí la señal", signal
+          , "(NUM DE SEÑAL), de mi padre PID", os.getppid(), "(PID INICIAL DEL PROCESO PADRE).")
+
+
+# Defino "function" para realizar todas las operaciones...
+def function():
+    # Primero en la variable "childnum" voy a alojar el valor que ingresamos como argumento, alojado en la variable proc.
+    childnum = func()
+
+    # Aca traemos el pid que le corresponde al padre y lo alojamos en la variable.
+    idfath = os.getpid()
+    pidchild = []
+
+    # Por ultimo realizamos un for, con un range(Valor ingresado como argumento)
+    for i in range(childnum):
+
+        # Hacemos la bifurcacion de los procesos hijos
+        son = os.fork()
+        if son == 0:
+            # Utilizamos signal.signal para el registro de señales
+            signal.signal(signal.SIGUSR2, handler)
+            signal.pause()
+            break
+
+        print("Creando proceso:", son)
+        pidchild.append(son)
+
+    if os.getpid() == idfath:
+        signals(pidchild)
+
+# Iniciamos el programa...
+function()
