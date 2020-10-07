dda06b8ce77b501e98c7e9f5230825ecf060683d
Giancarlo <gianca9405@gmail.com>
2020-10-07 12:33:03 -0300

    ejercicio 23 / reformado el 22 Alice

--- eje23ex22Alice.py
+++ eje23ex22Alice.py
@@ -1,10 +1,34 @@
 #!/usr/bin/python
 
-import socket, sys
+import socket, sys , threading
+from datetime import dtime
 
-# Primero definimos 2 funciones 1 para escuchar y otra para decir, luego una funcion para entablecer la conexion y un main
+def attend(Csocket, n, bloq):
 
-def hear(Csocket):
+    # En la variable Datime alojamos, la fecha y la hora
+    Datime = dtime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    # Abrimos el archivo "eje23ex19_LOG.txt" en el directorio "etc_23"
+    with open("etc_23/eje23ex22Alice_LOG.txt", "a") as file_LOG:
+        bloq.acquire()
+        # Alojamos el nombre del hilo en la variabl
+        Threading = threading.current_thread().name
+        # Escribimos en el archivo los logs, tanto de la fecha y hora que se ejecuto, y el nombre del hilo que atendio a bob
+        file_LOG.write(f"[{Datime}] Bob was taken care of by the thread: {Threading}\n")
+        # Arrancamos el bloqueo
+        bloq.release()
+
+    # Un while para:
+    while True:
+        # Escuchar
+        hear(Csocket)
+        hear(Csocket, n)
+        # Decir
+        tell(Csocket)
+        tell(Csocket, n)
+
+
+def hear(Csocket, n):
 
     while True:
         # Alojamos en la variable MsgClient el mensaje desencodeado que se envio desde "ALICE"
@@ -17,14 +41,14 @@ def hear(Csocket):
         elif MsgClient == "EXIT":
             sys.exit(0)
         # Printeamos el mensaje de Bob
-        print("Bob says: ", MsgClient)
+        print("Bob says ", {n}, ":", MsgClient)
 
 
-def tell(Csocket):
+def tell(Csocket, n):
 
     while True:
         # Alojamos en la variable MsgServer el mensaje encodeado que va a enviar "ALICE"
-        MsgServer = input("").encode()
+        MsgServer = input(f"Bob {n}:").encode()
         # Le enviamos al socket del cliente, el mensaje de ALICE
         Csocket.send(MsgServer)
 
@@ -57,11 +81,30 @@ def AcceptConnection():
 
 
 def main():
-    # Establecemos conexion entre los 2 socket, tanto cliente como servidor.
-    Ssocket, Csocket = AcceptConnection()
+    # n = cantidad de bobs o cliente que se conectan
+    n = 1; port = 7666
+
+    # Creamos el socket con protocolo TCP
+    Ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Asociamos el socket con la ip y el puerto
+    Ssocket.bind(('', port))
+    # Definimos la ip local sobre el cual vamos a correr el servidor, o Alice
+    IpLocalServer = socket.gethostbyname(socket.getfqdn())
+    bloq = threading.Lock()
+    print("Alice is online with ip: ", IpLocalServer, " and port: ", port)
+    # El socket del servidor, queda esperando una conexion
+    Ssocket.listen(16)
 
     while True:
-        hear(Csocket)
-        tell(Csocket)
+        # Aceptamos la conexion
+        Csocket, IpClient = Ssocket.accept()
+        print(IpClient, " conected...")
+
+        # Creamos la variable threading targeteando la funcion attend
+        threading = threading.Thread(target=attend, args=(Csocket, n, bloq))
+        threading.start()
+        # Informamos que se arranco un nuevo hilo
+        print(f"Start a new thread: {threading.name} for bob {n}")
+        n += 1
 
 main()
